00f31829e33fbeb486427420ecdc75d0d2aadd9a
oliverlevay <oliver.levay@hotmail.com>
2022-05-15 15:35:11 +0200

    finish backend

--- backend/src/routes/root.ts
+++ backend/src/routes/root.ts
@@ -14,7 +14,7 @@ enum GameState {
 
 let gameState: GameState = GameState.NotStarted;
 let randomUsers: Map<string, User> = new Map();
-const users: User[] = [];
+let users: User[] = [];
 
 let usersFinishedWithStep = 0;
 
@@ -23,7 +23,7 @@ const shuffleUsers = () => {
   if (users.length === 1) {
     idMap.set(users[0].id, users[0]);
   } else {
-    let hasntBeenPicked = users;
+    let hasntBeenPicked = [...users];
     for (let i: number = 0; i < users.length; i++) {
       const pickableId = hasntBeenPicked.filter((j) => j.id !== users[i].id);
       const pickedId =
@@ -41,7 +41,7 @@ const shuffleUsers = () => {
 };
 
 // userId, sentence pairs
-const sentences: Map<string, string> = new Map();
+const sentences: Map<string, string[]> = new Map();
 
 const glues = [
   'will',
@@ -72,13 +72,13 @@ const nouns = [
   'a phone',
   'Rome',
 ];
-/* const time = [
+const time = [
   'today',
   'tomorrow',
   'in the future',
   'soon',
   'in a couple of hours',
-]; */
+];
 // takes a list of words and returns 4 random ones with their index.
 
 const gameInfo = () => {
@@ -132,7 +132,7 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     },
     async (request, reply) => {
       if (gameState !== GameState.NotStarted) {
-        return { ...gameInfo() };
+        return { ...gameInfo(), error: 'Game has already started' };
       } else {
         const body = request.body as { name: string };
         const user = {
@@ -141,7 +141,9 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
           isAdmin: users.length === 0,
         };
         users.push(user);
-        console.log(`Register ${user.name} with id ${user.id}. He/she is ${user.isAdmin} admin`);
+        console.log(
+          `Register ${user.name} with id ${user.id}. He/she is ${user.isAdmin} admin`
+        );
         reply.code(201).send({ user, ...gameInfo() });
       }
     }
@@ -164,7 +166,7 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     },
     async (request, reply) => {
       if (gameState !== GameState.NotStarted) {
-        return { ...gameInfo() };
+        return { ...gameInfo(), error: 'Game has already started' };
       }
       const body = request.body as { id: string };
       if (!users.find((user) => user.id === body.id)?.isAdmin) {
@@ -173,7 +175,7 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
       gameState = GameState.Step1;
       shuffleUsers();
       usersFinishedWithStep = 0;
-      console.log(gameInfo);
+      console.log(gameInfo());
       reply.code(200).send({ ...gameInfo() });
     }
   );
@@ -195,16 +197,16 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     },
     async (request, reply) => {
       if (gameState !== GameState.Step1) {
-        return { ...gameInfo() };
+        return { ...gameInfo(), error: 'Game is not in step 1' };
       }
       const body = request.body as { id: string };
       const id = body.id;
       const ids = users.map((user) => user.id);
       if (!ids.includes(id)) {
-        console.log(`There is no user with ${id}`)
+        console.log(`There is no user with ${id}`);
         reply.badRequest('User not found');
       } else {
-        console.log(`The user with id ${id} got the words`)
+        console.log(`The user with id ${id} got the words`);
         reply
           .code(201)
           .send({ wordList: generateWordList(glues), ...gameInfo() });
@@ -232,23 +234,25 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     },
     async (request, reply) => {
       if (gameState !== GameState.Step1) {
-        return { ...gameInfo() };
+        return { ...gameInfo(), error: 'Game is not in step 1' };
       }
       const body = request.body as { id: string; index: number };
       const ids = users.map((user) => user.id);
       if (!ids.includes(body.id)) {
-        console.log(`There is no user with ${body.id}`)
+        console.log(`There is no user with ${body.id}`);
         reply.badRequest('User not found in current game.');
       }
       const randomUser = randomUsers.get(body.id)!;
-      const sentence = `${randomUser.name} ${glues[body.index]}`;
-      sentences.set(randomUser.id, sentence);
+      sentences.set(randomUser.id, [randomUser.name, glues[body.index]]);
+      usersFinishedWithStep += 1;
       if (usersFinishedWithStep === users.length) {
         gameState = GameState.Step2;
         usersFinishedWithStep = 0;
         shuffleUsers();
       }
-      console.log(`The user with ${body.id} sent the word ${glues[body.index]}`);
+      console.log(
+        `The user with ${body.id} sent the word ${glues[body.index]}`
+      );
       reply.code(201).send({ ...gameInfo() });
     }
   );
@@ -270,16 +274,16 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     },
     async (request, reply) => {
       if (gameState !== GameState.Step2) {
-        reply.badRequest('Waiting for other players...');
+        return { ...gameInfo(), error: 'Game is not in step 1' };
       }
       const body = request.body as { id: string };
       const id = body.id;
       const ids = users.map((user) => user.id);
       if (!ids.includes(id)) {
-        console.log(`There is no user with ${body.id}`)
+        console.log(`There is no user with ${body.id}`);
         reply.badRequest('User not found in current game.');
       } else {
-        console.log(`The user with id ${id} got the words`)
+        console.log(`The user with id ${id} got the words`);
         reply
           .code(201)
           .send({ wordList: generateWordList(verbs), ...gameInfo() });
@@ -307,23 +311,26 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     },
     async (request, reply) => {
       if (gameState !== GameState.Step2) {
-        return { ...gameInfo() };
+        return { ...gameInfo(), error: 'Game is not in step 2' };
       }
       const body = request.body as { id: string; index: number };
       const ids = users.map((user) => user.id);
       if (!ids.includes(body.id)) {
-        console.log(`There is no user with ${body.id}`)
+        console.log(`There is no user with ${body.id}`);
         reply.badRequest('User not found in current game.');
       }
       const randomUser = randomUsers.get(body.id)!;
-      const sentence = `${sentences.get(randomUser.id)} ${verbs[body.index]}`;
-      sentences.set(randomUser.id, sentence);
+      const existingWords = sentences.get(randomUser.id)!;
+      sentences.set(randomUser.id, [...existingWords, verbs[body.index]]);
+      usersFinishedWithStep += 1;
       if (usersFinishedWithStep === users.length) {
         gameState = GameState.Step3;
         usersFinishedWithStep = 0;
         shuffleUsers();
       }
-      console.log(`The user with ${body.id} sent the word ${glues[body.index]}`);
+      console.log(
+        `The user with ${body.id} sent the word ${glues[body.index]}`
+      );
       reply.code(201).send({ ...gameInfo() });
     }
   );
@@ -345,16 +352,16 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     },
     async (request, reply) => {
       if (gameState !== GameState.Step3) {
-        return { ...gameInfo() };
+        return { ...gameInfo(), error: 'Game is not in step 3' };
       }
       const body = request.body as { id: string };
       const id = body.id;
       const ids = users.map((user) => user.id);
       if (!ids.includes(id)) {
-        console.log(`There is no user with ${body.id}`)
+        console.log(`There is no user with ${body.id}`);
         reply.badRequest('User not found in current game.');
       } else {
-        console.log(`The user with id ${id} got the words`)
+        console.log(`The user with id ${id} got the words`);
         reply
           .code(201)
           .send({ wordList: generateWordList(nouns), ...gameInfo() });
@@ -382,24 +389,148 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     },
     async (request, reply) => {
       if (gameState !== GameState.Step3) {
-        return { ...gameInfo() };
+        return { ...gameInfo(), error: 'Game is not in step 3' };
       }
       const body = request.body as { id: string; index: number };
       const ids = users.map((user) => user.id);
       if (!ids.includes(body.id)) {
-        console.log(`There is no user with ${body.id}`)
+        console.log(`There is no user with ${body.id}`);
         reply.badRequest('User not found in current game.');
       }
       const randomUser = randomUsers.get(body.id)!;
-      const sentence = `${sentences.get(randomUser.id)} ${nouns[body.index]}`;
-      sentences.set(randomUser.id, sentence);
+      const existingWords = sentences.get(randomUser.id)!;
+      sentences.set(randomUser.id, [...existingWords, nouns[body.index]]);
+      usersFinishedWithStep += 1;
+      if (usersFinishedWithStep === users.length) {
+        gameState = GameState.Step4;
+        usersFinishedWithStep = 0;
+        shuffleUsers();
+      }
+      console.log(
+        `The user with ${body.id} sent the word ${glues[body.index]}`
+      );
+      reply.code(201).send({ ...gameInfo() });
+    }
+  );
+
+  fastify.post(
+    '/get-words/step4',
+    {
+      schema: {
+        body: {
+          type: 'object',
+          required: ['id'],
+          properties: {
+            id: {
+              type: 'string',
+            },
+          },
+        },
+      },
+    },
+    async (request, reply) => {
+      if (gameState !== GameState.Step4) {
+        return { ...gameInfo(), error: 'Game is not in step 4' };
+      }
+      const body = request.body as { id: string };
+      const id = body.id;
+      const ids = users.map((user) => user.id);
+      if (!ids.includes(id)) {
+        console.log(`There is no user with ${body.id}`);
+        reply.badRequest('User not found in current game.');
+      } else {
+        console.log(`The user with id ${id} got the words`);
+        reply
+          .code(201)
+          .send({ wordList: generateWordList(time), ...gameInfo() });
+      }
+    }
+  );
+
+  fastify.post(
+    '/select-word/step4',
+    {
+      schema: {
+        body: {
+          type: 'object',
+          required: ['index', 'id'],
+          properties: {
+            index: {
+              type: 'number',
+            },
+            id: {
+              type: 'string',
+            },
+          },
+        },
+      },
+    },
+    async (request, reply) => {
+      if (gameState !== GameState.Step4) {
+        return { ...gameInfo(), error: 'Game is not in step 4' };
+      }
+      const body = request.body as { id: string; index: number };
+      const ids = users.map((user) => user.id);
+      if (!ids.includes(body.id)) {
+        console.log(`There is no user with ${body.id}`);
+        reply.badRequest('User not found in current game.');
+      }
+      const randomUser = randomUsers.get(body.id)!;
+      const existingWords = sentences.get(randomUser.id)!;
+      sentences.set(randomUser.id, [...existingWords, time[body.index]]);
+      usersFinishedWithStep += 1;
       if (usersFinishedWithStep === users.length) {
         gameState = GameState.Finished;
         usersFinishedWithStep = 0;
         shuffleUsers();
       }
-      console.log(`The user with ${body.id} sent the word ${glues[body.index]}`);
+      console.log(
+        `The user with ${body.id} sent the word ${glues[body.index]}`
+      );
       reply.code(201).send({ ...gameInfo() });
+    }
+  );
+
+  fastify.get('/sentences', async (request, reply) => {
+    const res: string[] = [];
+    sentences.forEach((v) => {
+      res.push(v.join(' '));
+    });
+    if (gameState !== GameState.Finished) {
+      return { ...gameInfo(), error: 'Game is not finished', sentences: res };
+    }
+    return { ...gameInfo(), sentences: res };
+  });
+
+  fastify.post(
+    '/reset',
+    {
+      schema: {
+        body: {
+          type: 'object',
+          required: ['id'],
+          properties: {
+            id: {
+              type: 'string',
+            },
+          },
+        },
+      },
+    },
+    async (request, reply) => {
+      if (gameState !== GameState.Finished) {
+        return { ...gameInfo(), error: 'Game is not finished' };
+      }
+      const body = request.body as { id: string };
+      if (!users.find((user) => user.id === body.id)?.isAdmin) {
+        reply.badRequest('Only admins can reset the game');
+      }
+      gameState = GameState.NotStarted;
+      users = [];
+      sentences.clear();
+      usersFinishedWithStep = 0;
+      console.log('Game reset', gameInfo());
+      reply.code(200).send({ ...gameInfo() });
     }
   );
 };
